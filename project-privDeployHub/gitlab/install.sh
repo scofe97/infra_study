6372de2da265af37dce196aae0898dcd53730c3b
root <root@k8s-master.kubernetes.lab>
2024-03-10 10:29:24 +0000

    https 매니페스트 추가

--- project-privDeployHub/gitlab/install.sh
+++ project-privDeployHub/gitlab/install.sh
@@ -4,9 +4,9 @@
 SCRIPT_DIR=$(dirname "$(readlink -f "$0")")
 cd "$SCRIPT_DIR" || exit
 
-# kubectl create ns gitlab
-# kubectl config set-context --current --namespace=gitlab
-# helm install gitlab . --namespace gitlab -f my-values.yaml
+kubectl create ns gitlab
+kubectl config set-context --current --namespace=gitlab
+helm install gitlab . --namespace gitlab -f my-values.yaml
 
 sudo curl -sSL -o /usr/local/bin/argocd https://github.com/argoproj/argo-cd/releases/latest/download/argocd-linux-amd64
 sudo chmod +x /usr/local/bin/argocd
